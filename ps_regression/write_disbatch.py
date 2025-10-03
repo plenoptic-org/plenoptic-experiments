aceb57113216e272adc758680819f9475fdeda97
William F. Broderick <billbrod@gmail.com>
2025-10-03 13:20:00 -0400

    updates ps_regression

--- ps_regression/write_disbatch.py
+++ ps_regression/write_disbatch.py
@@ -2,12 +2,12 @@
 import pathlib
 import sys
 
-old_dir = pathlib.Path(sys.argv[1])
-new_dir = pathlib.Path(sys.argv[2])
+new_dir = pathlib.Path(sys.argv[1])
 out_dir = new_dir.parent / "figs"
 out_dir.mkdir(exist_ok=True)
 
 prepend = "PYTORCH_KERNEL_CACHE_PATH=~/.cache/torch/kernels TORCH_HOME=~/.cache/torch MPLCONFIGDIR=~/.cache/matplotlib PLENOPTIC_CACHE_DIR=~/.cache/plenoptic"
+old_dir = pathlib.Path("/mnt/home/wbroderick/.cache/plenoptic/ps_regression.tar.gz.untar/")
 
 commands = []
 for f in old_dir.glob("ps*pt"):
@@ -15,10 +15,10 @@ for f in old_dir.glob("ps*pt"):
     cmd = f"{prepend} python plot.py {f} {out_file} &> {out_file.with_suffix('.log')}"
     commands.append(cmd)
 
-for f in new_dir.glob("ps*pt"):
-    out_file = out_dir / ('new_' + f.with_suffix('.svg').name)
-    cmd = f"{prepend} python plot.py {f} {out_file} &> {out_file.with_suffix('.log')}"
-    commands.append(cmd)
+# for f in new_dir.glob("ps*pt"):
+#     out_file = out_dir / ('new_' + f.with_suffix('.svg').name)
+#     cmd = f"{prepend} python plot.py {f} {out_file} &> {out_file.with_suffix('.log')}"
+#     commands.append(cmd)
 
 with open("disbatch.txt", "w") as f:
     f.write("\n".join(commands))
