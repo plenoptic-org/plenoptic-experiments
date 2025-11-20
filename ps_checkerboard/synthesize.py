2603f6a8cc1da7b8c5ecc23833d7bce7494e0f91
William F. Broderick <billbrod@gmail.com>
2025-11-20 18:50:16 -0500

    switch to reweighting test

--- ps_checkerboard/synthesize.py
+++ ps_checkerboard/synthesize.py
@@ -10,6 +10,7 @@ import imageio.v3 as iio
 import pandas as pd
 
 def main(
+    img: str = "checkerboard",
     seed: int = 0,
     max_iter: int = 20,
     max_eval: int | None = None,
@@ -17,8 +18,17 @@ def main(
     line_search_fn: Literal[None, "strong_wolfe"] = None,
     device: str | int = 0,
     synth_max_iter: int = 200,
+    range_lmbda: float = .1,
+    mn: float = 1.,
+    mag_std: float = 1.,
+    autocorr_recon: float = 1.,
+    skew_recon: float = 1.,
+    kurt_recon: float = 1.,
+    cross_ori: float = 1.,
+    cross_scale_mag: float = 1.,
+    cross_scale_real: float = 1.,
+    autocorr_mag: float = 1.,
     lr: float = 0.01,
-    coarse_to_fine: int = 0,
     output_dir: str = "."
 ):
     output_dir = pathlib.Path(output_dir)
@@ -28,7 +38,12 @@ def main(
     except:
         pass
     device = torch.device(device)
-    img = po.load_images("checkerboard.jpg")
+    IMG_PATH = fetch_data("portilla_simoncelli_images.tar.gz")
+    fn = img
+    try:
+        img = po.load_images(IMG_PATH / f"{img}.jpg")
+    except FileNotFoundError:
+        img = po.load_images(f"{img}.jpg")
     img = img.to(device).to(torch.float64)
     model = po.simul.PortillaSimoncelli(img.shape[-2:], spatial_corr_width=7)
     model.to(device)
@@ -38,36 +53,34 @@ def main(
         # reweight the pixel min/max and the variance of the highpass residuals, since
         # they're weird.
         weights["pixel_statistics"][..., -2:] = 0
-    k = "var_highpass_residual"
-    if k in weights:
-        weights[k] = 100 * torch.ones_like(weights[k])
+        weights["pixel_statistics"][..., 0] = mn
+    weight_dict = {"var_highpass_residual": 100, "magnitude_std": mag_std,
+                   "auto_correlation_reconstructed": autocorr_recon,
+                   "skew_reconstructed": skew_recon, "kurtosis_reconstructed": kurt_recon,
+                   "cross_orientation_correlation_magnitude": cross_ori,
+                   "cross_scale_correlation_magnitude": cross_scale_mag,
+                   "cross_scale_correlation_real": cross_scale_real,
+                   "auto_correlation_magnitude": autocorr_mag}
+    for k, v in weight_dict.items():
+        weights[k] = v * torch.ones_like(weights[k])
     weights = model.convert_to_tensor(weights)
     nan_mask = model(img).isnan()
 
     def loss(x, y):
+        y = y.clone()
         x[nan_mask] = 0
         y[nan_mask] = 0
         return po.tools.l2_norm(weights * x, weights * y)
 
-    if coarse_to_fine:
-        def loss(x, y):
-            x[y.isnan()] = 0
-            y[y.isnan()] = 0
-            return po.tools.l2_norm(x, y)
-        met = po.synth.MetamerCTF(img, model, loss_function=loss,)
-        synth_kwargs = {"ctf_iters_to_check": coarse_to_fine}
-    else:
-        met = po.synth.Metamer(img, model, loss_function=loss,)
-        synth_kwargs = {}
+    met = po.synth.Metamer(img, model, loss_function=loss, allowed_range=(img.min(), img.max()),
+                           range_penalty_lambda=range_lmbda)
+    synth_kwargs = {}
     opt_kwargs = {"max_iter": max_iter, "max_eval": max_eval,
                   "history_size": history_size, "line_search_fn": line_search_fn,
                   "lr": lr}
     met.setup(optimizer=torch.optim.LBFGS, optimizer_kwargs=opt_kwargs)
     start = time.time()
     met.synthesize(max_iter=synth_max_iter, **synth_kwargs)
-    # then ctf synth hasn't finished moving through the scales
-    if coarse_to_fine and met.scales != ("all",):
-        met.synthesize(max_iter=synth_max_iter, **synth_kwargs)
     stop = time.time()
     met.save(output_dir / "metamer.pt")
     width_ratios = {"plot_representation_error": 3.1}
@@ -75,12 +88,40 @@ def main(
     fig.savefig(output_dir / f"metamer.svg")
     plt.close(fig)
     iio.imwrite(output_dir / "metamer.png", po.tools.data.convert_float_to_int(po.to_numpy(met.metamer.clip(0, 1)).squeeze()))
-    data = {"seed": seed, "search_func": line_search_fn,
+    data = {"image": fn, "seed": seed, "search_func": line_search_fn,
             "LBFGS_max_iter": max_iter, "LBFGS_max_eval": max_eval,
             "LBFGS_history_size": history_size, "learning_rate": lr, "synth_iter": len(met.losses),
-            "loss": met.losses[-1].item(), "synth_duration": stop-start,
-            "coarse_to_fine": coarse_to_fine}
-    df = pd.DataFrame(data, index=[0])
+            "synth_loss": met.losses[-1].item(), "synth_duration": stop-start}
+    data.update(weight_dict)
+    data["mean_weight"] = mn
+
+    def mse(x, y):
+        return (x-y).pow(2).nanmean().item()
+    def sse(x, y):
+        return (x-y).pow(2).nansum().item()
+    def l2_norm(x, y):
+        return (x-y).pow(2).nansum().sqrt().item()
+
+    df = []
+    rep = model(met.image)
+    met_rep = model(met.metamer)
+    rep_dict = model.convert_to_dict(rep)
+    met_rep_dict = model.convert_to_dict(met_rep)
+
+    for func in [mse, sse, l2_norm]:
+        d = data.copy()
+        d["loss"] = func(rep, met_rep)
+        d["loss_type"] = "overall"
+        d["loss_func"] = func.__name__
+        df.append(d)
+        for k in rep_dict.keys():
+            d = data.copy()
+            d["loss"] = func(rep_dict[k], met_rep_dict[k])
+            d["loss_type"] = k
+            d["loss_func"] = func.__name__
+            df.append(d)
+
+    df = pd.DataFrame(df)
     df.to_csv(output_dir / "loss.csv", index=False)
     torch.save(met.model(met.metamer), output_dir / "rep.pt")
 
@@ -89,6 +130,7 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="Run some PortillaSimoncelli synthesis to understand checkerboard synth",
     )
+    parser.add_argument("--img", "-i", default="checkerboard")
     parser.add_argument("--seed", "-s", default=0, type=int)
     parser.add_argument("--max_iter", default=20, type=int)
     parser.add_argument("--max_eval", default=None)
@@ -98,7 +140,15 @@ if __name__ == "__main__":
     parser.add_argument("--synth_max_iter", default=200, type=int)
     parser.add_argument("--lr", default=.01, type=float)
     parser.add_argument("--output_dir", '-o', default=".")
-    parser.add_argument("--coarse_to_fine", type=int, default=0)
+    parser.add_argument("--mn", type=float, default=1.)
+    parser.add_argument("--mag_std", type=float, default=1.)
+    parser.add_argument("--autocorr_recon", type=float, default=1.)
+    parser.add_argument("--skew_recon", type=float, default=1.)
+    parser.add_argument("--kurt_recon", type=float, default=1.)
+    parser.add_argument("--cross_ori", type=float, default=1.)
+    parser.add_argument("--cross_scale_mag", type=float, default=1.)
+    parser.add_argument("--cross_scale_real", type=float, default=1.)
+    parser.add_argument("--autocorr_mag", type=float, default=1.)
     args = vars(parser.parse_args())
     try:
         args["max_eval"] = int(args["max_eval"])
--- ps_checkerboard/write_disbatch.py
+++ ps_checkerboard/write_disbatch.py
@@ -2,22 +2,34 @@ import sys
 import itertools
 from pathlib import Path
 
-base_out = Path("/mnt/ceph/users/wbroderick/plenoptic_experiments/ps_checkerboard")
+base_out = Path("/mnt/ceph/users/wbroderick/plenoptic_experiments/ps_checkerboard_weights")
+base_out.mkdir(exist_ok=True)
 
-synth_max_iter= 2000
+synth_max_iter= 500
 device= 'cpu'
 
+img = ["checkerboard", "fig4a"]
 seed= [0]
 line_search_fn= ["strong_wolfe"]
-max_iter= [5, 10, 20, 40]
-max_eval = [.5, 1, None]
+max_iter= [10]
+max_eval = [1]
 history_size= [100]
 lrate = [1]
-coarse_to_fine = [0, 5, 10, 30, 50]
+mn = [10]
+mag_std = [10]
+autocorr_recon = [1., 10, 100]
+skew_recon = [0, .1, 1.]
+kurt_recon = [0, .1, 1.]
+cross_ori = [0, .1, 1.]
+cross_scale_mag = [0, .1, 1.]
+cross_scale_real = [0, 1., 1]
+autocorr_mag = [0, .1, 1.]
 
 prepend = "PYTORCH_KERNEL_CACHE_PATH=~/.cache/torch/kernels TORCH_HOME=~/.cache/torch MPLCONFIGDIR=~/.cache/matplotlib PLENOPTIC_CACHE_DIR=~/.cache/plenoptic OMP_NUM_THREADS=1"
 
-iters = itertools.product(seed, line_search_fn, max_iter, max_eval, history_size, lrate, coarse_to_fine)
+iters = itertools.product(img, seed, line_search_fn, max_iter, max_eval, history_size,
+                          lrate, mn, mag_std, autocorr_recon, skew_recon, kurt_recon,
+                          cross_ori, cross_scale_mag , cross_scale_real, autocorr_mag,)
 
 if len(sys.argv) > 1:
     fn = sys.argv[1]
@@ -25,13 +37,15 @@ else:
     fn = "disbatch.txt"
 
 commands = []
-for s, l, it, m, h, lr, ctf in iters:
-    outdir = f"seed-{s}_search-{l}_iter-{it}_eval-{m}_history-{h}_lr-{lr}_ctf-{ctf}"
+for im, s, l, it, m, h, lr, m_, st, acr, sr, kr, co, csm, csr, acm in iters:
+    outdir = f"img-{im}_seed-{s}_search-{l}_iter-{it}_eval-{m}_history-{h}_lr-{lr}_mn-{m_}_mt-{st}_acr-{acr}_sr-{sr}_kr-{kr}_co-{co}_csm-{csm}_csr-{csr}_acm-{acm}"
     if m is not None:
         m = int(m * it)
-    cmd = (f"{prepend} python ~/plenoptic_experiments/ps_checkerboard/synthesize.py -s {s} -d {device} "
+    cmd = (f"{prepend} python ~/plenoptic_experiments/ps_checkerboard/synthesize.py -i {im} -s {s} -d {device} "
            f"--max_iter {it} --max_eval {m} --line_search_fn {l} --synth_max_iter {synth_max_iter} "
-           f"-o {base_out / outdir} --lr {lr} --history_size {h} --coarse_to_fine {ctf}")
+           f"-o {base_out / outdir} --lr {lr} --history_size {h} --mn {m_} --mag_std {st} --autocorr_recon {acr} "
+           f"--skew_recon {sr} --kurt_recon {kr} --cross_ori {co} --cross_scale_mag {csm} --cross_scale_real {csr} "
+           f"--autocorr_mag {acm}")
     cmd = f"({cmd}) &> {base_out / outdir}.log"
     commands.append(cmd)
 
