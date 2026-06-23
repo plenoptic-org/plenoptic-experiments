ffc139559309b65d6778a4f7c72b2e7f829be7d1
William F. Broderick <billbrod@gmail.com>
2026-06-23 14:48:26 -0400

    adds init style

--- deepnet_metamer/write_disbatch.py
+++ deepnet_metamer/write_disbatch.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 base_out = Path("/mnt/ceph/users/wbroderick/plenoptic_experiments/deepnet_metamer/")
 
-seeds = [0, 1]
+seeds = [0]
 it = 12000
 
 device = "cpu"
@@ -30,8 +30,9 @@ lr = [1e-3, 3e-3, 3e-2, 1e-2, 1e-1]
 plot_cmds = []
 synth_cmds = []
 images = ["macaque"]
-for s, sch, o, loss, l, lay, img in itertools.product(seeds, scheduler, optimizer, loss_func, lr, layer, images):
-    outfile = base_out / f"model-ResNet50-{lay}_img-{img}_{device}_seed-{s}_iter-{it}_loss-{loss}_opt-{o}_lr-{l:.1e}_sch-{sch}.pt"
+init_style = ["full"]
+for s, sch, o, loss, l, lay, img, init in itertools.product(seeds, scheduler, optimizer, loss_func, lr, layer, images, init_style):
+    outfile = base_out / f"model-ResNet50-{lay}_img-{img}_init-{init}_{device}_seed-{s}_iter-{it}_loss-{loss}_opt-{o}_lr-{l:.1e}_sch-{sch}.pt"
     if outfile.exists() or outfile.with_name(outfile.name.replace("cpu", "0")).exists() or outfile.with_name(outfile.name.replace("_0_", "_cpu_")).exists():
         cmd = f"python plot.py --loss {loss} --layer {lay} --image {img} --plot -f {outfile}"
         cmd = f"{prefix} {cmd}"
